eee6405b79a2c01a61b29ae6f275979ca4e9e888
Zuhal 'Alimul Hadi <zuhalalimulhadi19@gmail.com>
2022-09-09 05:12:14 +0700

    feat: add breadcrumb component

---
+++ src/components/UI/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Breadcrumb as B, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
+
+import "./Breadcrumb.css";
+
+function Breadcrumb({ links }) {
+  return (
+    <B spacing='8px' separator={<img src="/icons/ic_breadcrumb.svg" alt="breadcrumb" />}>
+      {
+        links.map((link)=>(
+          <BreadcrumbItem key={link.id}>
+            <BreadcrumbLink href={links.href} isCurrentPage={link.isCurrentPage}>
+              <p className={link.isCurrentPage ? 'text-breadcrumb bold' : 'text-xs bold'}>
+                {link.label}
+              </p>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        ))
+      }
+    </B>
+  )
+}
+
+export default Breadcrumb
