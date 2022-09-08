5933f68e15e507763e00efea9a299f7c56cb0ec9
tyoprataama <tyoprataama004@gmail.com>
2022-09-08 18:42:54 +0700

    build footer logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navbar from './pages/Navbar/Navbar';
+import Footer from './pages/Footer/Footer';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Footer/>
     </>
   );
 }
---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer () {
+    return (
+        <footer className='footer'>
+            <div className='footer-wrap-left'>
+            <img className='footer-logo' src='https://i.ibb.co/Tq1KQJc/packo-logo-white.png' alt='footer-logo'/>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
