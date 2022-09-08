813786a32dc2c5fcf0a388f9daa734d4243b3443
aryakpt <aryakrisna07@gmail.com>
2022-09-09 01:21:24 +0700

    create assets for icon and data dummy

---
+++ src/assets/icons/index.js
@@ -0,0 +1,4 @@
+import ic_add from './icon-add.png';
+import ic_close from './icon-close.png';
+
+export { ic_add, ic_close };
---
+++ src/json/index.js
@@ -0,0 +1,3 @@
+import dummyData from './DummyData.json';
+
+export { dummyData };
