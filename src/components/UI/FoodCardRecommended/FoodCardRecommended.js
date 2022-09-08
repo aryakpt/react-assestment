eb5587e11e1139444b172a5eee323f0b508db887
aryakpt <aryakrisna07@gmail.com>
2022-09-09 01:27:39 +0700

    integration with modal

--- src/components/UI/FoodCardRecommended/FoodCardRecommended.js
+++ src/components/UI/FoodCardRecommended/FoodCardRecommended.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FoodCardRecommended.module.css';
-import { AddButton } from '../';
+import { AddButton, FoodExpanded } from '../';
 
 const FoodCardRecommended = ({ imgSrc, imgAlt, title, price }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  const onShowHandler = () => {
+    setIsShow(true);
+  };
+
+  const onCloseHandler = () => {
+    setIsShow(false);
+  };
+
   return (
-    <div className={styles.card}>
-      <img className={styles['card-img-top']} src={imgSrc} alt={imgAlt} />
-      <div className={styles['card-body']}>
-        <h5 className={styles['card-title']}>{title}</h5>
-        <div className={styles['card-body__bottom']}>
-          <p className={styles['card-text']}>{price}</p>
-          <AddButton />
+    <>
+      {isShow && (
+        <FoodExpanded
+          imgSrc={imgSrc}
+          imgAlt={imgAlt}
+          title={title}
+          price={price}
+          onClose={onCloseHandler}
+        />
+      )}
+      <div className={styles.card} onClick={onShowHandler}>
+        <img className={styles['card-img-top']} src={imgSrc} alt={imgAlt} />
+        <div className={styles['card-body']}>
+          <h5 className={styles['card-title']}>{title}</h5>
+          <div className={styles['card-body__bottom']}>
+            <p className={styles['card-text']}>{price}</p>
+            <AddButton />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/UI/FoodCardReguler/FoodCardReguler.js
+++ src/components/UI/FoodCardReguler/FoodCardReguler.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './FoodCardReguler.module.css';
-import { AddButton } from '../';
-import { imgDefault } from '../../../assets';
+import { AddButton, FoodExpanded } from '../';
 
-const FoodCardReguler = () => {
+const FoodCardReguler = ({ imgSrc, imgAlt, title, price }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  const onShowHandler = () => {
+    setIsShow(true);
+  };
+
+  const onCloseHandler = () => {
+    setIsShow(false);
+  };
+
   return (
-    <div className={styles.card}>
-      <img className={styles['card-img']} src={imgDefault} alt={'asdsad'} />
-      <div className={styles['card-body']}>
-        <h6 className={styles['card-title']}>Bubur Sarang Telor Setengah</h6>
-        <div className={styles['card-body__bottom']}>
-          <p className={styles['card-text']}>32.00</p>
-          <AddButton />
+    <>
+      {isShow && (
+        <FoodExpanded
+          imgSrc={imgSrc}
+          imgAlt={imgAlt}
+          title={title}
+          price={price}
+          onClose={onCloseHandler}
+        />
+      )}
+      <div className={styles.card} onClick={onShowHandler}>
+        <img className={styles['card-img']} src={imgSrc} alt={imgAlt} />
+        <div className={styles['card-body']}>
+          <h6 className={styles['card-title']}>{title}</h6>
+          <div className={styles['card-body__bottom']}>
+            <p className={styles['card-text']}>{price}</p>
+            <AddButton />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
