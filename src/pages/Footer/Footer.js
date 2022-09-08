0561c6e25f3472f558aaa4a5b45a459aa475170a
tyoprataama <tyoprataama004@gmail.com>
2022-09-08 18:59:46 +0700

    build footer desc

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -5,7 +5,9 @@ function Footer () {
     return (
         <footer className='footer'>
             <div className='footer-wrap-left'>
-            <img className='footer-logo' src='https://i.ibb.co/Tq1KQJc/packo-logo-white.png' alt='footer-logo'/>
+            <img className='footer-logo' src='https://i.ibb.co/Tq1KQJc/packo-logo-white.png' alt='logo'/>
+            <p className='footer-title'>Pemadam kelaparan ketika kamu mager rumah</p>
+            <p className='footer-desc'> Scelerisque platea pulvinar neque egestas et vivamus praesent porttitor libero.</p>
             </div>
         </footer>
     )
