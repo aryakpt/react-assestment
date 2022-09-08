b7b9bac8023f5b107a326a6f70c6f409d0d92d72
Zuhal 'Alimul Hadi <zuhalalimulhadi19@gmail.com>
2022-09-09 05:26:09 +0700

    fix: change breadcrumb component to arrow function component

--- src/components/UI/Breadcrumb/Breadcrumb.js
+++ src/components/UI/Breadcrumb/Breadcrumb.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import { Breadcrumb as B, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
+import React from 'react';
+import { Breadcrumb as B, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 
-import "./Breadcrumb.css";
+import './Breadcrumb.css';
 
-function Breadcrumb({ links }) {
+const Breadcrumb = ({ links }) => {
   return (
-    <B spacing='8px' separator={<img src="/icons/ic_breadcrumb.svg" alt="breadcrumb" />}>
-      {
-        links.map((link)=>(
-          <BreadcrumbItem key={link.id}>
-            <BreadcrumbLink href={links.href} isCurrentPage={link.isCurrentPage}>
-              <p className={link.isCurrentPage ? 'text-breadcrumb bold' : 'text-xs bold'}>
-                {link.label}
-              </p>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-        ))
-      }
+    <B spacing='8px' separator={<img src='/icons/ic_breadcrumb.svg' alt='breadcrumb' />}>
+      {links.map((link) => (
+        <BreadcrumbItem key={link.id}>
+          <BreadcrumbLink href={links.href} isCurrentPage={link.isCurrentPage}>
+            <p className={link.isCurrentPage ? 'text-breadcrumb bold' : 'text-xs bold'}>
+              {link.label}
+            </p>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      ))}
     </B>
-  )
-}
+  );
+};
 
-export default Breadcrumb
+export default Breadcrumb;
